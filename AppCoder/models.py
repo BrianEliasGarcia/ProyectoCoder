f95eefd350c6654aa2fa68f1d0255f4cb31e69c2
Brian Garcia <brianeliasgarcia25@gmail.com>
2023-03-17 14:40:28 -0300

    Este es mi primer commit estable

---
+++ AppCoder/models.py
@@ -0,0 +1,29 @@
+from django.db import models
+
+class Curso(models.Model):
+    nombre = models.CharField(max_length=40)
+    camada = models.IntegerField(Unique=True)
+
+    def __str__(self):
+        return f"curso: {self.nombre}, camada:{self.camada}"
+
+class Estudiantes(models.Model):
+    nombre = models.CharField(max_length=30)
+    apellido = models.CharField(max_length=30)
+    email = models.EmailField()
+
+
+class Profesor(models.Model):
+    nombre = models.CharField(max_length=30)
+    apellido = models.CharField(max_length=30)
+    email = models.EmailField()
+    profesion = models.CharField(max_length=30)
+
+    def __str__(self):
+        return f"Profesor: {self.nombre}, {self.apellido}"
+
+
+class Entregable(models.Model):
+    nombre = models.CharField(max_length=30)
+    fecha_de_entreg = models.DateField()
+    entregado = models.BooleanField()
